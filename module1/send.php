d889df8eccb8a00d7941fdda4d9e8790f3a027d7
Azamat Zorkanov <zorkanov.93@gmail.com>
2016-09-18 10:37:59 +0600

    email send function adde

--- module1/send.php
+++ module1/send.php
@@ -3,25 +3,83 @@ require_once('helpers/protect_from_guest.php');
 
 require_once('helpers/setsession.php');
 
+require_once('helpers/dbconnect.php');
+
 if(!isset($_SESSION['user_id'])){
 	header('Location: index.php');
 	exit();
 }
 
 if(isset($_POST) && !empty($_POST)){
-	$contacts=stripcslashes($_POST['contacts']);
+	$contact_list_id=stripcslashes($_POST['contact_list_id']);
 	$title=stripcslashes($_POST['title']);
 	$desc=stripcslashes($_POST['desc']);
-	
+
+
+	$stmt=$db->prepare("SELECT email from contacts WHERE contact_list_id=?");
+
+	$stmt->execute([
+		$contact_list_id
+	]);
+
+	$contacts=[];
+	while($contact=$stmt->fetch(PDO::FETCH_OBJ)){
+		$contacts[]=$contact;
+	}
+
+	$stmt=$db->prepare("INSERT INTO sent_emails(contact_id,event_date,status) VALUES(?,?,?)");
+
+	foreach($contacts as $contact){
+		$values=[
+			$contact->id,
+			time()
+		];
+		if(mail($contact->email,$title,$desc)){
+			$values[]='S';
+		}else{
+			$values[]='NS';
+		}
+		$stmt->execute($values);
+	}
+
+	/*$emails=join(",",array_map(function($elem){
+		return $elem->email;
+	}, $contacts));
+
+
+ 
+	if(mail($emails,$title,$desc)){
+		die('pk');
+	}else{
+		die('n ');
+	}*/
+
+
 }
 
+$stmt=$db->prepare("SELECT * from contact_list WHERE user_id=?");
+$stmt->execute([
+	$_SESSION['user_id']
+]);
+
+$contact_lists=[];
+while($contact_list=$stmt->fetch(PDO::FETCH_OBJ)){
+	$contact_lists[]=$contact_list;
+}
 ?>
 
 <?php require_once('layouts/header.php');?>
 	<form action="" method="POST">
 		<div class="row">
 			<div class="col-sm-4 col-md-4">
-				
+				<div class="form-group">
+					<label for="contact_list_id">Список контактов</label>
+					<select name="contact_list_id" id="contact_list_id" class="form-control">
+						<?php foreach($contact_lists as $contact_list){?>
+							<option value="<?= $contact_list->id?>"><?= $contact_list->name?></option>
+						<?php } ?>
+					</select>
+				</div>
 			</div>
 			<div class="col-sm-8 col-md-8">
 				<div class="form-group">
